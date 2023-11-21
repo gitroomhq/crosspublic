557fad3cd1de0c68a6fd91f3b81365c71d95a655
Nevo David <nevo@notu.co>
2023-11-21 16:32:07 +0700

    feat: no log

--- libraries/database/src/prisma.service.ts
+++ libraries/database/src/prisma.service.ts
@@ -3,6 +3,11 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log: []
+    });
+  }
   async onModuleInit() {
     await this['$connect']();
   }
