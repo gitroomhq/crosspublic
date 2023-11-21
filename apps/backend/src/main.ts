ebb0ce0ae9fc71d826c4304b310cd99422c5e7f3
Nevo David <nevo@notu.co>
2023-11-22 01:29:55 +0700

    feat: api and serverkey

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
       res.header('Access-Control-Allow-Origin', allowedOrigin);
       res.header('Access-Control-Allow-Credentials', 'true');
       res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, apikey, serverkey');
     }
 
     if (req.method === 'OPTIONS') {
