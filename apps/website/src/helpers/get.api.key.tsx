f99cf2283c32fba7754292ab6f8e8cbc872cf6ba
Nevo David <nevo@notu.co>
2023-11-22 13:51:20 +0700

    feat: fix subdomain check

--- apps/website/src/helpers/get.api.key.tsx
+++ apps/website/src/helpers/get.api.key.tsx
@@ -1,17 +1,8 @@
 import {customFetchBackend} from "@meetqa/helpers/src/fetchObject/custom.fetch.backend";
 import * as process from "process";
 
-function isSubdomain(url: string) {
-  // Remove protocols, paths, queries, and fragments
-  const domain = url.replace(/(https?:\/\/)?(www\.)?/, '').split(/[/?#]/)[0];
-
-  // Split the domain by dots and check if there are at least 3 parts (e.g., sub.domain.com)
-  const parts = domain.split('.');
-  return parts.length > 2;
-}
-
 export const publicRequestFetch = async (domain: string) => {
-  const getDomainSubdomain = isSubdomain(domain) ? {subdomain: domain.split('.')[0].replace('www', '')} : {domain};
+  const getDomainSubdomain = domain.indexOf('.') ? {subdomain: domain.split('.')[0].replace('www', '')} : {domain};
   const {data: {apiKey}} = await customFetchBackend().get(
     `/public/organization?${new URLSearchParams(getDomainSubdomain as never).toString()}`,
     {cache: 'force-cache'}
