3ff9395534972355dde3c5dda7226b3d5cf97576
Nevo David <nevo@notu.co>
2023-11-22 13:39:36 +0700

    feat: force cache

--- apps/website/src/helpers/get.api.key.tsx
+++ apps/website/src/helpers/get.api.key.tsx
@@ -14,7 +14,7 @@ export const publicRequestFetch = async (domain: string) => {
   const getDomainSubdomain = isSubdomain(domain) ? {subdomain: domain.split('.')[0].replace('www', '')} : {domain};
   const {data: {apiKey}} = await customFetchBackend().get(
     `/public/organization?${new URLSearchParams(getDomainSubdomain as never).toString()}`,
-    {cache: 'no-store'}
+    {cache: 'force-cache'}
   );
 
   return {request: customFetchBackend(undefined, {apikey: apiKey, serverkey: process.env.BACKEND_TOKEN_PROTECTOR})};
