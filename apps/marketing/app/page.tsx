9c96490f89f382ef81344aa6943d7fb1be36cace
Nevo David <nevo@notu.co>
2023-11-24 00:52:48 +0700

    feat: comment

--- apps/marketing/app/page.tsx
+++ apps/marketing/app/page.tsx
@@ -1,8 +1,8 @@
 import HeroSection from "@meetqa/marketing/components/HeroSection";
 import Features from "@meetqa/marketing/components/Features";
-import Solution from "@meetqa/marketing/components/Solution";
+// import Solution from "@meetqa/marketing/components/Solution";
 import Reviews from "@meetqa/marketing/components/Reviews";
-import Princing from "@meetqa/marketing/components/Princing";
+// import Princing from "@meetqa/marketing/components/Princing";
 import {Metadata} from "next";
 
 export const metadata: Metadata = {
