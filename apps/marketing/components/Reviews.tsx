567c691de6c85e3e73153f40045252ed5c6104c0
Nevo David <nevo@notu.co>
2023-11-27 12:47:37 +0700

    feat: trusted by developers at

--- apps/marketing/components/Reviews.tsx
+++ apps/marketing/components/Reviews.tsx
@@ -1,8 +1,23 @@
 import Container from "./Container";
 import Card from "./Card";
 import Image from "next/image.js";
+import {chunk} from "lodash";
+
+interface Review {
+    name: string;
+    role: string;
+    avatar: string;
+    title: string;
+    content: string;
+}
+const reviewsList: Review[] = [
+];
+
+const chunkIt = chunk(reviewsList, 3);
 
 export default function Reviews() {
+    if (!reviewsList.length) return (<></>);
+
     return (
         <div className="pt-32 md:pt-20 text-gray-600 dark:text-gray-300" id="reviews">
             <Container>
@@ -10,92 +25,24 @@ export default function Reviews() {
                     <h2 className="text-center text-3xl font-bold text-gray-900 dark:text-white md:text-4xl lg:text-5xl">Trusted by developers</h2>
                 </div>
                 <div className="mt-12 md:mt-20 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-                    <div className="space-y-6">
-                        <Card>
-                            <div className="flex gap-4">
-                                <Image className="h-12 w-12 rounded-full" src="/images/avatars/avatar.webp" alt="user avatar" width="400" height="400" loading="lazy" />
-                                <div>
-                                    <h3 className="text-lg font-medium text-gray-700 dark:text-white">Daniella Doe</h3>
-                                    <p className="text-sm text-gray-500 dark:text-gray-300">Mobile dev</p>
-                                </div>
-                            </div>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum aliquid quo eum quae quos illo earum ipsa doloribus nostrum minus libero aspernatur laborum cum, a suscipit, ratione ea totam ullam! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto laboriosam deleniti aperiam ab veniam sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit, veritatis harum et rerum.</p>
-                        </Card>
-                        <Card>
-                            <div className="flex gap-4">
-                                <Image className="h-12 w-12 rounded-full" src="/images/avatars/avatar-1.webp" alt="user avatar" width="400" height="400" loading="lazy" />
-                                <div>
-                                    <h3 className="text-lg font-medium text-gray-700 dark:text-white">Daniella Doe</h3>
-                                    <p className="text-sm text-gray-500 dark:text-gray-300">@hundler</p>
-                                </div>
-                            </div>
-                            <p>Illum aliquid quo eum quae quos illo laboriosam deleniti aperiam ab veniam sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit, veritatis harum et rerum.</p>
-                        </Card>
-                    </div>
-                    <div className="space-y-6">
-                        <Card>
-                            <div className="flex gap-4">
-                            <Image className="h-12 w-12 rounded-full" src="/images/avatars/avatar-1.webp" alt="user avatar" width="400" height="400" loading="lazy" />
-                                <div>
-                                    <h3 className="text-lg font-medium text-gray-700 dark:text-white">Daniella Doe</h3>
-                                    <p className="text-sm text-gray-500 dark:text-gray-300">Mobile dev</p>
-                                </div>
-                            </div>
-                            <p>Nostrum minus libero sit amet consectetur, adipisicing elit ab veniam sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit, veritatis harum et rerum.</p>
-                        </Card>
-                        <Card>
-                            <div className="flex gap-4">
-                                <Image className="h-12 w-12 rounded-full" src="/images/avatars/avatar-3.webp" alt="user avatar" width="400" height="400" loading="lazy" />
-                                <div>
-                                    <h3 className="text-lg font-medium text-gray-700 dark:text-white">Daniella Doe</h3>
-                                    <p className="text-sm text-gray-500 dark:text-gray-300">Mobile dev</p>
-                                </div>
-                            </div>
-                            <p>Nostrum minus libero sit amet consectetur, adipisicing elit ab veniam sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit, veritatis harum et rerum.</p>
-                        </Card>
-                        <Card>
-                            <div className="flex gap-4">
-                                <Image className="h-12 w-12 rounded-full" src="/images/avatars/avatar-2.webp" alt="user avatar" width="400" height="400" loading="lazy" />
-                                <div>
-                                    <h3 className="text-lg font-medium text-gray-700 dark:text-white">Daniella Doe</h3>
-                                    <p className="text-sm text-gray-500 dark:text-gray-300">@hundler</p>
-                                </div>
-                            </div>
-                            <p>Illum aliquid quo eum quae quos illo laboriosam deleniti aperiam ab veniam sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit, veritatis harum et rerum.</p>
-                        </Card>
-                    </div>
-                    <div className="space-y-6 sm:hidden lg:block">
-                        <Card>
-                            <div className="flex gap-4">
-                                <Image className="h-12 w-12 rounded-full" src="/images/avatars/avatar.webp" alt="user avatar" width="400" height="400" loading="lazy" />
-                                <div>
-                                    <h3 className="text-lg font-medium text-gray-700 dark:text-white">Daniella Doe</h3>
-                                    <p className="text-sm text-gray-500 dark:text-gray-300">Mobile dev</p>
-                                </div>
-                            </div>
-                            <p>Architecto laboriosam. Sint libero voluptas veniam at reprehenderit, veritatis harum et rerum.</p>
-                        </Card>
-                        <Card>
-                            <div className="flex gap-4">
-                                <Image className="h-12 w-12 rounded-full" src="/images/avatars/avatar-4.webp" alt="user avatar" width="400" height="400" loading="lazy" />
-                                <div>
-                                    <h3 className="text-lg font-medium text-gray-700 dark:text-white">Daniella Doe</h3>
-                                    <p className="text-sm text-gray-500 dark:text-gray-300">@hundler</p>
-                                </div>
-                            </div>
-                            <p>Illum aliquid quo deleniti aperiam ab veniam sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit, veritatis harum et rerum.</p>
-                        </Card>
-                        <Card>
-                            <div className="flex gap-4">
-                                <Image className="h-12 w-12 rounded-full" src="/images/avatars/avatar.webp" alt="user avatar" width="400" height="400" loading="lazy" />
-                                <div>
-                                    <h3 className="text-lg font-medium text-gray-700 dark:text-white">Daniella Doe</h3>
-                                    <p className="text-sm text-gray-500 dark:text-gray-300">@hundler</p>
-                                </div>
-                            </div>
-                            <p>Illum aliquid quo deleniti aperiam ab veniam Sint libero voluptas veniam at reprehenderit, veritatis harum et rerum.</p>
-                        </Card>
-                    </div>
+                    {chunkIt.map((reviews, index) => (
+                        <div className="space-y-6" key={`review-` + index}>
+                            {reviews?.map(r => (
+                                <Card key={r.name}>
+                                    <div className="flex gap-4">
+                                        <Image className="h-12 w-12 rounded-full" src={r.avatar} alt="user avatar" width="400" height="400" loading="lazy" />
+                                        <div>
+                                            <h3 className="text-lg font-medium text-gray-700 dark:text-white">{r.name}</h3>
+                                            <p className="text-sm text-gray-500 dark:text-gray-300">{r.role}</p>
+                                        </div>
+                                    </div>
+                                    <p>
+                                        {r.content}
+                                    </p>
+                                </Card>
+                            ))}
+                        </div>
+                    ))}
                 </div>
             </Container>
         </div>
