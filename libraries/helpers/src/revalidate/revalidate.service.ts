8245f29051c792e6fdb7b0e1059bdfe0d2150b17
Nevo David <nevo@notu.co>
2023-11-29 12:02:42 +0700

    feat: revalidate url

--- libraries/helpers/src/revalidate/revalidate.service.ts
+++ libraries/helpers/src/revalidate/revalidate.service.ts
@@ -4,7 +4,7 @@ import {Injectable} from "@nestjs/common";
 @Injectable()
 export class RevalidateService {
   revalidate(name: string) {
-    return axios.post(process.env.FRONTEND_URL + '/api/revalidate', {
+    return axios.post(process.env.REVALIDATE_URL + '/api/revalidate', {
       name,
     }, {
       headers: {
